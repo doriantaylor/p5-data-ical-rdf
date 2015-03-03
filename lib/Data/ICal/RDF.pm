db72d6b7c2608a17fe71c3a9d900c1b4ccc79b8f
Dorian Taylor <dorian.taylor.lists@gmail.com>
2015-03-03 13:32:05 -0800

    initial import

--- lib/Data/ICal/RDF.pm
+++ lib/Data/ICal/RDF.pm
@@ -1,12 +1,12 @@
 package Data::ICal::RDF;
 
-use 5.006;
+use 5.010;
 use strict;
-use warnings;
+use warnings FATAL => 'all';
 
 =head1 NAME
 
-Data::ICal::RDF - The great new Data::ICal::RDF!
+Data::ICal::RDF - Turn iCal files into an RDF graph
 
 =head1 VERSION
 
@@ -19,34 +19,46 @@ our $VERSION = '0.01';
 
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
-
-Perhaps a little code snippet.
-
     use Data::ICal::RDF;
 
-    my $foo = Data::ICal::RDF->new();
-    ...
+    # Instantiate a processing context with the appropriate handlers:
+    my $context = Data::ICal::RDF->new(
+        resolve_uid      => sub {
+            # returns an RDF node for the UID...
+        },
+        store_attachment => sub {
+            # stores the attachment somewhere and returns an
+            # identifier...
+        },
+        resolve_attachment => sub {
+            # resolves a relation between an attachment and its file
+            # name...
+        },
+    );
 
-=head1 EXPORT
+    # Process a Data::ICal object...
+    $context->process($ical);
 
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
+    # Successive calls to 'process' against different iCal objects
+    # will accumulate statements in the context's internal model.
 
-=head1 SUBROUTINES/METHODS
+    # Now you can do whatever you like with the model.
+    my $result = $context->model;
 
-=head2 function1
+=head1 METHODS
+
+=head2 new
 
 =cut
 
-sub function1 {
+sub BUILD {
 }
 
-=head2 function2
+=head2 process
 
 =cut
 
-sub function2 {
+sub process {
 }
 
 =head1 AUTHOR
@@ -55,12 +67,11 @@ Dorian Taylor, C<< <dorian at cpan.org> >>
 
 =head1 BUGS
 
-Please report any bugs or feature requests to C<bug-data-ical-rdf at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Data-ICal-RDF>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
+Please report any bugs or feature requests to C<bug-data-ical-rdf at
+rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Data-ICal-RDF>.  I
+will be notified, and then you'll automatically be notified of
+progress on your bug as I make changes.
 
 =head1 SUPPORT
 
@@ -91,26 +102,22 @@ L<http://search.cpan.org/dist/Data-ICal-RDF/>
 
 =back
 
-
-=head1 ACKNOWLEDGEMENTS
-
+=head1 SEE ALSO
 
 =head1 LICENSE AND COPYRIGHT
 
 Copyright 2015 Dorian Taylor.
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    L<http://www.apache.org/licenses/LICENSE-2.0>
+Licensed under the Apache License, Version 2.0 (the "License"); you
+may not use this file except in compliance with the License.  You may
+obtain a copy of the License at
+L<http://www.apache.org/licenses/LICENSE-2.0>.
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+implied.  See the License for the specific language governing
+permissions and limitations under the License.
 
 =cut
 
